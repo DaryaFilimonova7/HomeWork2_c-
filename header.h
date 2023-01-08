b005a9656b722bef81244a882a3276e8227b57c4
DaryaFilimonova7 <91047620+DaryaFilimonova7@users.noreply.github.com>
2023-01-08 06:17:31 +0300

    Add files via upload

---
+++ header.h
@@ -0,0 +1,23 @@
+//
+// Created by MSI on 08.01.2023.
+//
+
+#ifndef HOMEWORK2_HEADER_H
+#define HOMEWORK2_HEADER_H
+
+#endif //HOMEWORK2_HEADER_H
+
+#include <iostream>
+void swap(int *ptr1, int *ptr2);
+template <typename T>
+void insertion_sort(T arr[], int idx_start, int idx_end, int *ptr);
+template <typename T>
+void localmin(T arr[], int idx_start, int idx_end, int *ptrMin);
+template <typename T>
+void between_zero(T arr[], int idx_start, int idx_end, int *ptr);
+template <typename T>
+void change(T arr[], int idx_start, int idx_end);
+template <typename T>
+void bubble_sort(T arr[], int idx_start, int idx_end);
+template <typename T>
+void merger(T num1[],T num2[],int m,int n);
